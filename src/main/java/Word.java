5a087b8ae9570222cdfa5f46d590341460f606e1
MinhVu <nguyenvanminhvu@gmail.com>
2021-10-12 13:08:19 +0700

    Verison DataBase UPDATE

---
+++ src/main/java/Word.java
@@ -0,0 +1,58 @@
+public class Word {
+    private String id;
+    private String word;
+    private String html;
+    private String description;
+    private String pronounce;
+
+    public Word() {
+    }
+
+    public Word(String id, String word, String html, String description, String pronounce) {
+        this.id = id;
+        this.word = word;
+        this.html = html;
+        this.description = description;
+        this.pronounce = pronounce;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPronounce() {
+        return pronounce;
+    }
+
+    public void setPronounce(String pronounce) {
+        this.pronounce = pronounce;
+    }
+}
